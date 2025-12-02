78951f41693ec6f5901863240a635dead1f54c27
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2025-12-02 16:36:23 +0530

    feat: add `TearingAlgorithm` interface

--- src/tearing.jl
+++ src/tearing.jl
@@ -49,3 +49,71 @@ function contract_variables(graph::BipartiteGraph, var_eq_matching::Matching,
 
     return newgraph
 end
+
+"""
+    $TYPEDSIGNATURES
+
+Find the equations (source vertices of `graph`) which are not matched to a variable present
+in `vars_scc` according to the matching `var_eq_matching`. `varfilter` filters out
+variables to exclude from this process.
+"""
+function free_equations(graph::BipartiteGraph, vars_scc::Vector{Vector{Int}},
+                        var_eq_matching::Matching, varfilter::F) where {F}
+    ne = nsrcs(graph)
+    seen_eqs = falses(ne)
+    for vars in vars_scc, var in vars
+
+        varfilter(var) || continue
+        ieq = var_eq_matching[var]
+        if ieq isa Int
+            seen_eqs[ieq] = true
+        end
+    end
+    findall(!, seen_eqs)
+end
+
+const MatchingT{T} = Matching{T, Vector{Union{T, Int}}}
+const MatchedVarT = Union{Unassigned, SelectedState}
+const VarEqMatchingT = MatchingT{MatchedVarT}
+
+"""
+    $TYPEDEF
+
+A struct containing the results of tearing.
+
+# Fields
+
+$TYPEDFIELDS
+"""
+struct TearingResult
+    """
+    The variable-equation matching. Differential variables are matched to `SelectedState`.
+    The derivative of a differential variable is matched to the corresponding differential
+    equation. Solved variables are matched to the equation they are solved from. Algebraic
+    variables are matched to `unassigned`.
+    """
+    var_eq_matching::VarEqMatchingT
+    """
+    The variable-equation matching prior to tearing. This is the maximal matching used to
+    compute `var_sccs` (see below). For generating the torn system, `var_eq_matching` is
+    the source of truth. This should only be used to identify algebraic equations in each
+    SCC.
+    """
+    full_var_eq_matching::VarEqMatchingT
+    """
+    The partitioning of variables into strongly connected components (SCCs). The SCCs are
+    sorted in dependency order, so each SCC depends on variables in previous SCCs.
+    """
+    var_sccs::Vector{Vector{Int}}
+end
+
+"""
+    $TYPEDEF
+
+Supertype for all tearing algorithms. A tearing algorithm takes as input the
+`SystemStructure` along with any other necessary arguments.
+
+The output of a tearing algorithm must be a `TearingResult` and a `NamedTuple` of
+any additional data computed in the process that may be useful for further processing.
+"""
+abstract type TearingAlgorithm end
