17feec415648146f420df52b4de5be8809a9783d
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2026-02-18 11:52:05 +0530

    test: add test for `maybe_zeros`

--- lib/ModelingToolkitTearing/test/runtests.jl
+++ lib/ModelingToolkitTearing/test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using ModelingToolkitTearing
 using ModelingToolkit
+using Symbolics
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
 @testset "`InferredDiscrete` validation" begin
@@ -10,4 +11,15 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
     @test_throws ModelingToolkitTearing.ExpectedDiscreteClockPartitionError mtkcompile(sys)
     @named sys = System([x(k) ~ x(k-1) + x(k-2), D(y) ~ x + t], t)
     @test_throws ModelingToolkitTearing.ExpectedDiscreteClockPartitionError mtkcompile(sys)
+end
+
+@testset "`maybe_zeros` is respected" begin
+    @variables x(t) y(t)
+    @parameters p
+    @mtkcompile sys = System([D(x) ~ y, p * y ~ x], t)
+    @test issetequal(unknowns(sys), [x])
+    @test issetequal(observables(sys), [y])
+    @mtkcompile sys = System([D(x) ~ y, p * y ~ x], t; maybe_zeros = Symbolics.SymbolicT[p])
+    @test issetequal(unknowns(sys), [x, y])
+    @test isempty(observables(sys))
 end
